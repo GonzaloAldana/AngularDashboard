852cd1acf3c885b83741078d013e72c637eb4b7b
Gonzalo <gon.zalo.music97@gmail.com>
2020-06-20 23:09:09 -0500

    VS Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   API: {
     EndPoint: {
-      Northwind: 'http://192.168.1.75:54967/p2/GonzaloNoe/Northwind/',
+      Northwind: 'http://192.168.1.75/API.Northwind.ISSC811/p2/GonzaloNoe/Northwind/',
       Login: 'http://192.168.11.130:3000/'
     }
   }
